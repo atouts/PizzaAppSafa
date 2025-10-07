e5c780e9cb240b5e987dc2b5901758830b67bd84
safa- <atouts@sheridancollege.ca>
2025-10-07 15:42:30 -0400

    Prepare PizzaController

---
+++ PizzaAppSafa/Controllers/PizzaController.cs
@@ -0,0 +1,42 @@
+﻿//Safa Atout
+//Student#: 991620668
+
+using Microsoft.AspNetCore.Mvc;
+using PizzaAppSafa.BusinessLayer;
+using PizzaAppSafa.Models;
+
+namespace PizzaAppSafa.Controllers
+{
+    public class PizzaController : Controller
+    {
+
+        private readonly IPizzaService _pizzaService;
+
+        public PizzaController(IPizzaService pizzaService)
+        {
+            _pizzaService = pizzaService;
+        }
+
+        public IActionResult Index()
+        {
+            ViewBag.Message = "Welcome to Safa’s Pizza Corner!";
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Receipt(PizzaOrder order)
+        {
+
+            if (string.IsNullOrWhiteSpace(order.CustomerName) || order.Quantity <= 0)
+            {
+                ViewBag.Error = "Please fill in all fields correctly.";
+                return View("Index");
+            }
+
+
+            var receipt = _pizzaService.CalculateBill(order);
+
+
+            return View(receipt);
+        }
+    }
+}
